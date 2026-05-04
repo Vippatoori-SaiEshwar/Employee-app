fcacdf7e7f4393204602f5c6884065f96bce562d
Vippatoori Sai Eshwar <saieshwar.992006@gmail.com>
2026-05-04 22:38:41 +0530

    Fix deployment configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/Employee-app/' : '/',
+  parallel: false,
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  },
   transpileDependencies: true
 })
